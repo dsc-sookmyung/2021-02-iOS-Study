79099243254c1ba7e2508011329ff2e34cfd6cd1
nami-koko <namitech3@gmail.com>
2021-10-10 20:22:30 +0900

    practice auto layout

---
+++ TabBarController_with_storyboard/MyMainTabBarController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  TabBarController_with_storyboard
+//
+//  Created by 고나미 on 2021/10/07.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
