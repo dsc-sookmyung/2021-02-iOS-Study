347d82f4ac07138d5efa3a988c65e62fd19a8a16
whaeundo25 <1916388@sookmyung.ac.kr>
2021-10-10 23:52:40 +0900

    [Week02] Autolayout적용하기

---
+++ Week02-kde/Week02-kde/MyMainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Week02-kde
+//
+//  Created by doeun kim on 2021/10/10.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
