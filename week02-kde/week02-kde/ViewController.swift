77b3ed68c78e0fb53b515c31d2d2ae20f6f55d49
whaeundo25 <1916388@sookmyung.ac.kr>
2021-10-24 01:34:50 +0900

    [3주차] Modal, Navigation Controller / scrollview 실습

---
+++ week02-kde/week02-kde/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  week02-kde
+//
+//  Created by doeun kim on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userImage: UIButton!
+    @IBOutlet weak var userId: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+  
+    @IBOutlet weak var instagramImageView: UIImageView!
+    
+    @IBOutlet weak var heartButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
